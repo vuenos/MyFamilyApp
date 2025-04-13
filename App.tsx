e61923517b6090d7929c0af0eef5181efa31d377
Jintae kim <vuenos@naver.com>
2025-04-13 16:42:22 +0900

    before reset typeScript

--- App.tsx
+++ App.tsx
@@ -4,20 +4,47 @@
  *
  * @format
  */
-
 import React from 'react';
-import {ScrollView, StatusBar, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#61dafb',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Android 대응
+  },
+  block: {
+    paddingHorizontal: 24,
+  },
+  text: {
+    fontSize: 30,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+});
 
 function App(): React.JSX.Element {
   return (
-    <View>
-      <StatusBar barStyle="dark-content" />
-      <ScrollView>
-        <View>
-          <Text>Hello</Text>
-        </View>
-      </ScrollView>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <StatusBar barStyle="dark-content" />
+        <ScrollView>
+          <View style={styles.block}>
+            <Text style={styles.text}>React Native !!</Text>
+            <Text style={styles.text}>React Native !!</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,3 @@
 export default function HomeScreen() {
-  return (
-      <>
-        HomeScreen
-      </>
-  )
+  return <>HomeScreen?</>;
 }
